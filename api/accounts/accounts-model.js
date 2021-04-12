73cc14c76f35c8cd13ec50ff47e1b21655913578
Leon Nasswetter <leon.nasswetter@gmail.com>
2021-04-12 15:13:19 -0700

    almost mvp

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -1,22 +1,35 @@
+const db = require("../../data/db-config");
+
 const getAll = () => {
   // DO YOUR MAGIC
-}
+  return db("accounts");
+};
 
-const getById = id => {
+const getById = (id) => {
   // DO YOUR MAGIC
-}
+  return db("accounts").where("id", id).first();
+};
 
-const create = account => {
+const create = async (account) => {
   // DO YOUR MAGIC
-}
+  const trimmed = { ...account, name: account.name.trim() };
+  const newAccount = await db("accounts").insert(trimmed);
+  return await getById(newAccount);
+};
 
-const updateById = (id, account) => {
+const updateById = async (id, account) => {
   // DO YOUR MAGIC
-}
+  const trimmed = { ...account, name: account.name.trim() };
+  await db("accounts").where("id", id).update(trimmed);
+  return getById(id);
+};
 
-const deleteById = id => {
+const deleteById = async (id) => {
   // DO YOUR MAGIC
-}
+  const toBeChopped = await getById(id);
+  await db("accounts").where("id", id).delete();
+  return toBeChopped;
+};
 
 module.exports = {
   getAll,
@@ -24,4 +37,4 @@ module.exports = {
   create,
   updateById,
   deleteById,
-}
+};
